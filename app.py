53e3b0cbb2c4525d02f61b05ee9e39b3a23e67a2
Tara Denniston <taradenniston@gmail.com>
2023-01-31 14:51:43 -0500

    create user model

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, render_template
+from models import db, connect_db, User
 
 app = Flask(__name__)
+app.app_context().push()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'A2a`T2W8t@}#(tH8V2fg=bLC66e"|('
+
 
 connect_db(app)
 db.create_all()
+
+@app.route('/')
+def home_page():
+    """Shows home page""" 
+    return render_template('home.html')
