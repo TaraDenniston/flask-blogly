8cb4c588379045b5c868f95ed512845d0641d1ed
Tara Denniston <taradenniston@gmail.com>
2023-02-01 08:16:56 -0500

    create new user form

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from models import db, connect_db, User
 
 app = Flask(__name__)
@@ -19,3 +19,21 @@ def home_page():
     """Shows home page with list of users""" 
     users = User.query.all()
     return render_template('home.html', users=users)
+
+@app.route('/new-user')
+def add_form():
+    """Displays Create a User Form"""
+    return render_template('new-user.html')
+
+@app.route('/new-user', methods=['POST'])
+def add_user():
+    """Adds new user to database using form data"""
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+
+    user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(user)
+    db.session.commit()
+    
+    return redirect(f'/{user.id}')
