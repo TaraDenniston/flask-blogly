84689f6359a5051276cd49bd29c1fffe6b8c1173
Tara Denniston <taradenniston@gmail.com>
2023-02-06 08:57:53 -0500

    add user edit page

--- app.py
+++ app.py
@@ -15,17 +15,22 @@ connect_db(app)
 db.create_all()
 
 @app.route('/')
+def go_home():
+    """Redirect to list of users""" 
+    return redirect('/users')
+
+@app.route('/users')
 def home_page():
-    """Show home page with list of users""" 
+    """Show list of users""" 
     users = User.query.all()
     return render_template('home.html', users=users)
 
-@app.route('/new-user')
+@app.route('/users/new')
 def add_form():
     """Display Create a User Form"""
     return render_template('new-user.html')
 
-@app.route('/new-user', methods=['POST'])
+@app.route('/users/new', methods=['POST'])
 def add_user():
     """Add new user to database using form data"""
     first_name = request.form['first_name']
@@ -40,16 +45,44 @@ def add_user():
     # message to the user and redirect back to the form
     if User.query.filter_by(first_name=first_name, last_name=last_name).one_or_none():
         flash('A user already exists by that name')
-        return redirect('/new-user')
+        return redirect('/users/new')
 
     user = User(first_name=first_name, last_name=last_name, image_url=image_url)
     db.session.add(user)
     db.session.commit()
     
-    return redirect(f'/{user.id}')
+    return redirect(f'/users/{user.id}')
 
-@app.route('/<int:user_id>')
+@app.route('/users/<int:user_id>')
 def display_user(user_id):
     """Show details about a single user"""
     user = User.query.get_or_404(user_id)
     return render_template("user-detail.html", user=user)
+
+@app.route('/users/<int:user_id>/edit')
+def edit_form(user_id):
+    """Display Edit a User Form"""
+    return render_template('edit-user.html', user_id=user_id)
+
+@app.route('/users/<int:user_id>/edit', methods=['POST'])
+def edit_user(user_id):
+    """Make updates to the database for an existing user using form data"""
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+
+    user = User.query.get(user_id)
+
+    # Only update info for fields that were not blank
+    if first_name:
+        user.first_name = first_name
+    if last_name:
+        user.last_name = last_name
+    if image_url:
+        user.image_url = image_url
+
+    db.session.add(user)
+    db.session.commit()
+    
+    return redirect(f'/users/{user.id}')
+
